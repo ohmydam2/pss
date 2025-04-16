8bc3d4ecf4e7b1aa722c0af0be11e13c9910c50f
Patrick Avelar <patrickavelarsilva@gmail.com>
2025-04-15 21:57:37 -0300

    Created program skeleton

--- include/error.h
+++ include/error.h
@@ -1,12 +1,15 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#include "log.h"
+
 typedef enum Error {
-    ERROR_OK = 0
+    ERROR_OK = 0,
+    ERROR_TOO_FEW_ARGUMENTS
 } Error;
 
 
-#define PROPAGATE(__ERROR) do { Error error = ((__ERROR)); if (error != ERROR_OK) return error; } while (0)
+#define BURN(__TAG,__ERROR) do { Error error = ((__ERROR)); if (error != ERROR_OK) { LOGE(__TAG, error); return error; }} while(0)
 
 
 char const *errorToString(Error error);
--- source/error.c
+++ source/error.c
@@ -4,6 +4,8 @@ char const *errorToString(Error error) {
     switch (error) {
         case (ERROR_OK):
             return "ERROR_OK";
+        case (ERROR_TOO_FEW_ARGUMENTS):
+            return "ERROR_TOO_FEW_ARGUMENTS";
         default:
             return "INVALID ERROR CODE";
      }
